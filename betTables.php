8415f00651ad8dbb1af24dcdf7f9746597d670b0
bananaflame <bbukaty@aol.com>
2016-06-28 15:00:57 -0700

    put stuff back in /scripts instead of /dev

--- betTables.php
+++ betTables.php
@@ -5,22 +5,69 @@ $dbconn = pg_connect("host=localhost dbname=payback user=postgres password=pingp
 
 $debtsTable = 'debts';
 $historyTable = 'history';
-	
-if ($_POST['action'] == 'increment') {
-	$id = $_POST['id'];
-	$query = "UPDATE $debtsTable SET QUANTITY = QUANTITY + 1 WHERE ID = $id";
-	pg_query($query) or die('Query failed: ' . pg_last_error());
-}
+$usersTable = 'users';
 
-elseif ($_POST['action'] == 'decrement') {
-	$id = $_POST['id'];
-	$query = "UPDATE $debtsTable SET QUANTITY = QUANTITY - 1 WHERE ID = $id";
-	pg_query($query) or die('Query failed: ' . pg_last_error());
-}
-
-elseif($_POST['action'] == 'remove') {
-	$id = $_POST['id'];
+function returnTable() {
+	global $debtsTable,$historyTable,$usersTable;
 	
+	if ($_POST['action'] == 'getHistory') {
+		$query = "SELECT * FROM $historyTable ORDER BY BETDATE DESC, WINNER, LOSER;";
+		$history = true;
+	}
+	else {
+		$query = "SELECT * FROM $debtsTable ORDER BY BETDATE DESC, WINNER, LOSER;";
+		$history = false;
+	}
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+	echo "<table class='table'>",
+		"<thead>\n";
+		if ($history) {
+			echo "<tr><th>Date Removed</th><th>Winner</th><th>Loser</th><th>Item</th><th>#</th><th>Description</th>\n</tr>";
+		}
+		else {
+			echo "<tr><th>Date</th><th>Winner</th><th>Loser</th><th>Item</th><th>#</th><th>Description</th><th>Remove</th>\n</tr>";
+		}
+		echo "\n</thead>",
+			"\n<tbody>";
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		echo "\t<tr>\n";
+		$col_num = 0;
+		foreach ($line as $col_value) {
+			if ($col_num == 0) {}
+			elseif ($col_num == 1) {
+				$year = substr($col_value,0,4);
+				$date = substr($col_value,5,5);
+				$time = substr($col_value,11,5);
+				$timestamp = $time . " " . $date . "-" . $year;
+				echo "\t\t<td>$timestamp</td>\n";
+			}
+			elseif ($col_num == 5) {
+				if ($history) {
+					echo "\t\t<td>$col_value</td>\n";
+				}
+				else {
+					echo "\t\t<td id=\"quantity$line[id]\">$col_value     <button class=\"btn btn-primary btn-xs\" onclick=\"changeQuantity('$line[id]',$col_value,'none')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button></td>\n";
+				}
+			}
+			elseif ($col_num == 6 and !$history) {
+				echo "\t\t<td>$col_value</td>\n";
+				echo "\t\t<td><button class=\"btn btn-danger btn-xs\" onclick=\"removeBet($line[id],'none')\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"/></button></td>\n";
+				
+			}
+			else {
+				echo "\t\t<td>$col_value</td>\n";
+			}
+			$col_num++;
+		}
+		echo "\t</tr>\n";
+		}
+	echo "</tbody>\n</table>\n";
+		
+	pg_free_result($result);
+}
+function removeEntry($id) {
+	global $debtsTable,$historyTable,$usersTable;
 	$query = "INSERT INTO $historyTable (betdate,winner,loser,item,quantity,description) SELECT now(),winner,loser,item,quantity,description FROM $debtsTable where id=$id";
 	pg_query($query) or die('Query failed: ' . pg_last_error());
 	
@@ -28,6 +75,26 @@ elseif($_POST['action'] == 'remove') {
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	$resultarray = pg_fetch_array($result, null, PGSQL_ASSOC);
 	pg_free_result($result);
+	returnTable();
+}
+
+if ($_POST['action'] == 'getDebts' or $_POST['action'] == 'getHistory') {
+	returnTable();
+}
+
+elseif($_POST['action'] == 'remove') {
+	removeEntry($_POST['id']);
+}
+
+elseif ($_POST['action'] == 'updateQuantity') {
+	$id = $_POST['id'];
+	$newQuantity = $_POST['quantity'];
+	$query = "INSERT INTO $historyTable (betdate,winner,loser,item,quantity,description) SELECT now(),winner,loser,item,quantity,description||' [Quantity changed to $newQuantity]' FROM $debtsTable where id=$id";
+	pg_query($query) or die('Query failed: ' . pg_last_error());
+	
+	$query = "UPDATE $debtsTable SET QUANTITY = $newQuantity WHERE ID = $id";
+	pg_query($query) or die('Query failed: ' . pg_last_error());
+	returnTable();
 }
 
 elseif ($_POST['action'] == 'upload') {
@@ -40,69 +107,67 @@ elseif ($_POST['action'] == 'upload') {
 	$query = "INSERT INTO $debtsTable (BETDATE,WINNER,LOSER,ITEM,QUANTITY,DESCRIPTION) VALUES (NOW(),'$winner','$loser','$item','$quantity','$description')";
 	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 	pg_free_result($result);
+	returnTable();
 }
-else {}
+elseif ($_POST['action'] == 'getDropdown') {
 
-if ($_POST['action'] == 'getHistory') {
-	$query = "SELECT * FROM $historyTable ORDER BY BETDATE, WINNER, LOSER";
-	$history = true;
-}
-else {
-	$query = "SELECT * FROM $debtsTable ORDER BY WINNER, BETDATE, LOSER";
-	$history = false;
-}
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	$query = "SELECT * FROM $usersTable ORDER BY NAME";
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	
+	if ($_POST['dropdownLocation'] == 'navbar') {
+		while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+			$name = $line['name'];
+			echo "<li><a role=\"button\" onclick=\"displayUser('$name')\">$name</a></li>\n";
+		}
+	}
+	elseif ($_POST['dropdownLocation'] == 'betForm') {
+		while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+			$name = $line['name'];
+			echo "<option>$name</option>\n";
+		}
+	}
 
-echo "<table class='table'>",
-	"<thead>\n";
-	if ($history) {
-		echo "<tr><th>Date Removed</th><th>Winner</th><th>Loser</th><th>Item</th><th>Quantity</th><th>Description</th>\n</tr>";
+
+
+	pg_free_result($result);
+}
+elseif ($_POST['action'] == 'tryAddUser') {
+	$name = $_POST['name'];
+	$query = "SELECT * FROM $usersTable WHERE NAME='$name';";
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	if (pg_num_rows($result) == 0) {
+		$query = "INSERT INTO $usersTable (NAME) VALUES ('$name');";
+		$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+		pg_free_result($result);
+		echo "success";
 	}
 	else {
-		echo "<tr><th>Date</th><th>Winner</th><th>Loser</th><th>Item</th><th>Quantity</th><th>Description</th><th>Remove</th>\n</tr>";
+		echo "fail";
 	}
-	echo "\n</thead>",
-		"\n<tbody>";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-	echo "\t<tr>\n";
-	$col_num = 0;
-	foreach ($line as $col_value) {
-		if ($col_num == 0) {}
-		elseif ($col_num == 1) {
-			$year = substr($col_value,0,4);
-			$date = substr($col_value,5,5);
-			$time = substr($col_value,11,5);
-			$timestamp = $time . " " . $date . "-" . $year;
-			echo "\t\t<td>$timestamp</td>\n";
-		}
-		elseif ($col_num == 5) {
-			if ($history) {
-				echo "\t\t<td>$col_value</td>\n";
-			}
-			else {
-				echo "\t\t<td>$col_value   ",
-				"<div class=\"btn-group\" role=\"group\">",
-				"<button class=\"btn btn-primary btn-xs\" onclick=\"alterBet($line[id],'increment')\"><span class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\"></span></button>",
-				"<button class=\"btn btn-primary btn-xs\" onclick=\"alterBet($line[id],'decrement')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button>",
-				"</div></td>\n";
-			}
-		}
-		elseif ($col_num == 6 and !$history) {
-			echo "\t\t<td>$col_value</td>\n";
-			echo "\t\t<td><button class=\"btn btn-danger btn-xs\" onclick=\"alterBet($line[id],'remove')\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"/></button></td>\n";
-			
-		}
-		else {
-			echo "\t\t<td>$col_value</td>\n";
-		}
-		$col_num++;
+}
+elseif ($_POST['action'] == 'deleteUser') {
+	$name = $_POST['name'];
+	$query = "SELECT * FROM $debtsTable WHERE WINNER='$name' OR LOSER='$name';";
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	$resultArray = pg_fetch_all($result);
+	foreach ($resultArray as $bet) {
+		removeEntry($bet['id']);
 	}
-	echo "\t</tr>\n";
-	}
-echo "</tbody>\n</table>\n";
 	
-pg_free_result($result);
-
+	$query = "DELETE FROM $usersTable * WHERE NAME='$name';";
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+}
+elseif ($_POST['action'] == 'getUserTable') {
+	$name = $_POST['name'];
+	$query = "SELECT * FROM $debtsTable WHERE WINNER='$name' OR LOSER='$name';";
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+	$userTable = pg_fetch_all($result);
+	echo json_encode($userTable);
+	
+}
+else {
+	echo "Error: no POST action specified.";
+}
 
 pg_close($dbconn);
 ?>
--- scripts.js
+++ scripts.js
@@ -1,27 +1,162 @@
+
 function updateBoard(htmlData) {
 	var board = document.getElementById('boardDiv');
 	board.innerHTML = htmlData;
-	}
-function initializeBoard(boardName) {
-	if (boardName == "history") {
+}
+
+function getBoard(boardName) {
+	if (boardName == 'history') {
 		var actionToPost = "getHistory";
+		document.getElementById("homeNavbar").className = "";
+		document.getElementById("usersNavbar").className = "";
+		document.getElementById("historyNavbar").className += " active";
 	}
 	else {
-		var actionToPost = "null";
+		var actionToPost = "getDebts";
 	}
 	jQuery.post('betTables.php',{action: actionToPost},function (data) {
 		updateBoard(data);
 	});
 }
-function alterBet(betId,alteration) {
-	jQuery.post('betTables.php',{id: betId, action: alteration},function (data) {
-		updateBoard(data);
+
+function removeBet(betId,currentUser) {
+	jQuery.post('betTables.php',{action: 'remove', id:betId},function (data) {
+		if (currentUser == "none") {
+			updateBoard(data);
+		}
+		else {
+			displayUser(currentUser);
+		}
+	});
+}
+
+var none = "none";
+function changeQuantity(betId,originalQuantity,currentUser) {
+	quantity = document.getElementById("quantity"+betId);
+	quantityNum = parseInt(quantity.innerHTML);
+	quantityNum--;
+	quantity.className = "bg-danger";
+	quantity.innerHTML = quantityNum+"     <button class=\"btn btn-primary btn-xs\" onclick=\"changeQuantity("+betId+","+originalQuantity+",'"+currentUser+"')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button>";
+	quantity.innerHTML += "   <button class=\"btn btn-primary btn-xs\" onclick=\"uploadQuantity("+betId+","+quantityNum+",'"+currentUser+"')\"><span class=\"glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>  Save</button>";
+	quantity.innerHTML += "   <button class=\"btn btn-primary btn-xs\" onclick=\"cancelQuantity("+betId+","+originalQuantity+",'"+currentUser+"')\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>  Cancel</button>";
+}
+function cancelQuantity(betId,originalQuantity,currentUser) {
+	quantity = document.getElementById("quantity"+betId);
+	quantity.innerHTML = originalQuantity+"     <button class=\"btn btn-primary btn-xs\" onclick=\"changeQuantity("+betId+","+originalQuantity+",'"+currentUser+"')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button>";
+	quantity.className = "";
+}
+function uploadQuantity(betId,newQuantity,currentUser) {
+	jQuery.post('betTables.php',{action: 'updateQuantity', id:betId, quantity: newQuantity}, function (data) {
+		if (currentUser == "none") {
+			updateBoard(data);
+		}
+		else {
+			displayUser(currentUser);
+		}
+	});
+}
+
+function getUsersDropdowns() {
+	jQuery.post('betTables.php',{action: "getDropdown",dropdownLocation: "navbar"},function (data) {
+		var navbarUsersDropdown = document.getElementById('navbarUsersDropdown');
+		navbarUsersDropdown.innerHTML = data;
+	});
+	jQuery.post('betTables.php',{action: "getDropdown",dropdownLocation: "betForm"},function (data) {
+		var winnerEntryUsersDropdown = document.getElementById('winnerEntry');
+		var loserEntryUsersDropdown = document.getElementById('loserEntry');
+		var deleteUserEntryDropdown = document.getElementById('deleteUserEntry');
+		winnerEntryUsersDropdown.innerHTML = data;
+		loserEntryUsersDropdown.innerHTML = data;
+		deleteUserEntryDropdown.innerHTML = data;
+	});
+}
+
+function displayUser(userName) {
+	var boardDiv = document.getElementById('boardDiv');
+	boardDiv.innerHTML = "Loading user page...";
+	jQuery.post('betTables.php',{action: "getUserTable",name: userName},function (data) {
+		document.getElementById("homeNavbar").className = "";
+		document.getElementById("historyNavbar").className = "";
+		document.getElementById("usersNavbar").className += " active";
+		
+		boardDiv.innerHTML = "<div class=\"page-header\"><h2>User: "+userName+"</h2></div>";
+		
+		var userTable = JSON.parse(data);
+		if (!userTable.length > 0) {
+			boardDiv.innerHTML += "<h4>No bets found for this user.</h4>";
+			return;
+		}
+		
+		var others = [];
+		userTable.forEach(function (bet) {
+			if (bet.winner !=userName && (jQuery.inArray(bet.winner,others) == -1)) {
+				others.push(bet.winner);
+			}
+			else if (bet.loser != userName && (jQuery.inArray(bet.loser,others) == -1)) {
+				others.push(bet.loser);
+			}
+		});
+		others.sort();
+		
+		var currChunk = "";
+		var debtsToOther = [];
+		var debtsFromOther = [];
+		others.forEach(function (other) {
+			currChunk = "";
+			debtsToOther = [];
+			debtsFromOther = [];
+			currChunk += "<div class=\"row nameRow\"><div class=\"col-md-12\"><h3>"+other+"</h3></div></div>";
+			currChunk += "<div class=\"row spacedRow\">";
+			userTable.forEach(function (bet) {
+				if (bet.winner == other) {
+					debtsToOther.push(bet);
+				}
+				else if (bet.loser == other) {
+					debtsFromOther.push(bet);
+				}
+			});
+			currChunk += "<div class=\"col-md-6\">";
+			currChunk += "<h4>"+other+" owes "+userName+":</h4>";
+			if (debtsFromOther.length > 0) {
+				currChunk += "<table class=\"table\"><thead><tr><th>Date</th><th>Item</th><th>#</th><th>Description</th><th>Remove</th></thead>";
+				currChunk += "<tbody>";
+				debtsFromOther.forEach(function (debt) {
+					currChunk += "<tr><td>"+debt['betdate']+"</td><td>"+debt['item']+"</td><td id=\"quantity"+debt['id']+"\">";
+					currChunk += debt['quantity']+"     <button class=\"btn btn-primary btn-xs\" onclick=\"changeQuantity("+debt['id']+","+debt['quantity']+",'"+userName+"')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button>";
+					currChunk += "</td><td>"+debt['description']+"</td>";
+					currChunk += "<td><button class=\"btn btn-danger btn-xs\" onclick=\"removeBet("+debt['id']+",'"+userName+"')\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"/></button></td></tr>";
+				});
+				currChunk += "</tbody></table>";
+			}
+			currChunk += "</div>";
+			
+			currChunk += "<div class=\"col-md-6\">";
+			currChunk += "<h4>"+userName+" owes "+other+":</h4>";
+			if (debtsToOther.length > 0) {
+				currChunk += "<table class=\"table\"><thead><tr><th>Date</th><th>Item</th><th>#</th><th>Description</th><th>Remove</th></thead>";
+				currChunk += "<tbody>";
+				debtsToOther.forEach(function (debt) {
+					currChunk += "<tr><td>"+debt['betdate']+"</td><td>"+debt['item']+"</td><td id=\"quantity"+debt['id']+"\">";
+					currChunk += debt['quantity']+"     <button class=\"btn btn-primary btn-xs\" onclick=\"changeQuantity("+debt['id']+","+debt['quantity']+",'"+userName+"')\"><span class=\"glyphicon glyphicon-minus\" aria-hidden=\"true\"></span></button>";
+					currChunk += "</td><td>"+debt['description']+"</td>";
+					currChunk += "<td><button class=\"btn btn-danger btn-xs\" onclick=\"removeBet("+debt['id']+",'"+userName+"')\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"/></button></td></tr>";
+				
+				});
+				currChunk += "</tbody></table>";
+				
+			}
+			currChunk += "</div></div>";
+			
+			
+			boardDiv.innerHTML += currChunk;
+			
+		});
 	});
 }
 
 jQuery(document).ready(function () {
-	jQuery('form[name=betForm]').submit(function(event) {
-		event.preventDefault(); //prevents the default form submission routine
+	jQuery('form[id=betForm]').submit(function(betFormSubmit) {
+		betFormSubmit.preventDefault(); //prevents the default form submission routine
 		var winnerInput = document.getElementById("winnerEntry").value;
 		var loserInput = document.getElementById("loserEntry").value;
 		var itemInput = document.getElementById("itemEntry").value;
@@ -30,6 +165,10 @@ jQuery(document).ready(function () {
 		
 		if (!(Number.isInteger(parseInt(quantityInput)))) {
 			alert("Quantity must be an integer. Please try again.");
+			return;
+		}
+		else if (winnerInput == loserInput) {
+			alert("Bet winner and loser must be different people. Please try again.");
 			return;
 		}
 		
@@ -43,7 +182,41 @@ jQuery(document).ready(function () {
 			},
 			function (data) {
 				updateBoard(data);
+				jQuery('#collapseForm').collapse("hide");
 				document.getElementById("betForm").reset();
+				document.getElementById("historyNavbar").className = "";
+				document.getElementById("usersNavbar").className = "dropdown";
+				document.getElementById("homeNavbar").className = "active";
 			});
-	});		
+	});
+	jQuery('form[id=newUserForm]').submit(function(nameFormSubmit) {
+		nameFormSubmit.preventDefault();
+		var nameInput = document.getElementById("newUserEntry").value;
+		jQuery.post('betTables.php',{action: "tryAddUser", name: nameInput}, function (data) {
+			if (data == "success") {
+				jQuery('#newUserModal').modal('hide');
+				document.getElementById("newUserForm").reset();
+				getUsersDropdowns();
+			}
+			else {
+				document.getElementById("newUserForm").reset();
+				alert("This person is already listed as a user. Please try again.");
+			}
+		});
+	});
+	jQuery('form[id=deleteUserForm]').submit(function(deleteUserFormSubmit) {
+		deleteUserFormSubmit.preventDefault();
+		if (window.confirm("This will delete all existing bets involving this user and remove them from the user list. Are you sure you want to continue?")) {
+			var nameInput = document.getElementById("deleteUserEntry").value;
+			jQuery.post('betTables.php',{action: "deleteUser", name: nameInput}, function (data) {
+				getBoard("debts");
+				jQuery('#deleteUserModal').modal('hide');
+				document.getElementById("deleteUserForm").reset();
+				getUsersDropdowns();
+				document.getElementById("historyNavbar").className = "";
+				document.getElementById("usersNavbar").className = "dropdown";
+				document.getElementById("homeNavbar").className = "active";
+		});
+		}
+	});
 });
